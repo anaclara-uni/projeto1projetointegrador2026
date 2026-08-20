bd8c3bff33f04eee762d0258e4e6c25798f8fa5a
anaclara-uni <272368@unifio.edu.br>
2026-08-20 01:14:27 +0000

    aula2

---
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Categoria.java
@@ -0,0 +1,19 @@
+package br.edu.unifio.ecommerce.entidades;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+ //@NoArgsConstructor 
+ //@AllArgsConstructor
+// byte short int long Byte Short Integer Long
+// float double Float Double
+// char Character
+// boolean Boolean
+public class Categoria {
+    private Short id;
+    private String nome; 
+    private String descricao; 
+
+}
